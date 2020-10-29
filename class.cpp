009a8df4b84ae50671ad838968ac0816831fcac5
Dima <nikitin_dima200dn@mail.ru>
2020-10-29 14:00:48 +0300

    Adding gretting

---
+++ class.cpp
@@ -0,0 +1,32 @@
+include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class gretting
+{
+private:
+	string name;
+
+public:
+	
+	gretting(string name)
+	{
+		this->name = name;
+	}
+	string foo()
+	{
+		cout<< "Hello, " << name<<endl
+	}
+};
+
+
+
+int main()
+{
+	string x = "Dmitry";
+	gretting gret(x);
+	gret.foo();
+	return 0;
+}
