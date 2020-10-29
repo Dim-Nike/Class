d8234ba718b68632c0190e9b544d22dbf92a68dd
Dima <nikitin_dima200dn@mail.ru>
2020-10-29 22:06:04 +0300

    corrected v2

--- proect.cpp
+++ proect.cpp
@@ -8,68 +8,69 @@ string x;
 class gretting
 {
 private:
-	string name;
+    string name;
 
 public:
 
-	gretting(string name)
-	{
-		this->name = name;
-	}
-	void foo()
-	{
-		cout << "Hello, " << name << endl;
-	}
+    gretting(string name)
+    {
+        this->name = name;
+    }
+    void foo()
+    {
+        cout << "Hello, " << name << endl;
+    }
 };
 
 class regestr
 {
 private:
-	int x;
-	int y;
+    int x;
+    int y;
 
 public:
 
-	void head()
-	{
-		cout << "Enter login and password" << endl;
-		int x, y;
-		this->x = x;
-		this->y = y;
-		cin >> x >> y;
-		int login = x;
-		int password = y;
-		cout<<"Enter again login and password"<<endl;
-                cin>>x>>y;
-                if (login==x && password==y)
-                {
-                        cout<<"You passed the resolution"<<endl;
-                }
+    void head()
+    {
+        cout << "Enter login and password" << endl;
+        this->x = x;
+        this->y = y;
+        cin >> x >> y;
+        int login = x;
+        int password = y;
+        cout << "Enter again login and password" << endl;
+        cin >> x >> y;
+        if (login == x && password == y)
+        {
+            cout << "You passed the resolution" << endl;
+        }
 
-                else
-                {
-                        cout<<"Error:\nWrong login or password"<<endl;
-	}
-};
+        else
+        {
+            cout << "Error:\nWrong login or password" << endl;
+        }
+    };
 
-class bot
-{
+    /*class bot
+    {
 
-public:
-	void user()
-	{
-		cout << "Hello, i am bot, and you " << x << endl;
-	}
-};
+    public:
+        void user()
+        {
+            cout << "Hello, i am bot, and you " << x << endl;
+        }
+    };*/
 
-int main()
-{
-	cout << "How are you name?" << endl;
+    int main()
+    {
+        cout << "How are you name?" << endl;
+        string x;
+        cin >> x;
+        gretting gretting(x);
+        regestr v1;
+        v1.head();
 
-	cin >> x;
-	gretting gret(x);
-	gret.foo();
 
+        return 0;
+    }
 
-	return 0;
-}
